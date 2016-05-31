ce867254c292a71138e0c2297641723e6a567f5a
sophie.rubin@de.ibm.com <ibiucukd23r>
2016-05-31 13:40:49 +0200

    Das ist was ich bis jetzt habe...heute Abend versuche ich noch die Zwischenergebnisse in einem Container zu speichern und die Möglichkeit zu machen, dass nicht mit allen Würfeln weitergemacht wird.

---
+++ Kniffel/src/kniffel/Kniffel/Würfel.java
@@ -0,0 +1,61 @@
+package kniffel.Kniffel;
+
+public class Würfel {
+	
+	public int würfel1;
+	public int würfel2;
+	public int würfel3;
+	public int würfel4;
+	public int würfel5;
+	
+	public Würfel (int würfel1, int würfel2, int würfel3, int würfel4, int würfel5) {
+		setWürfel1 (würfel1);
+		setWürfel2 (würfel2);
+		setWürfel3 (würfel3);
+		setWürfel4 (würfel4);
+		setWürfel5 (würfel5);
+	}
+
+
+	public int getWürfel1() {
+		return würfel1;
+	}
+
+	public void setWürfel1(int würfel1) {
+		this.würfel1 = würfel1;
+	}
+
+	public int getWürfel2() {
+		return würfel2;
+	}
+
+	public void setWürfel2(int würfel2) {
+		this.würfel2 = würfel2;
+	}
+
+	public int getWürfel3() {
+		return würfel3;
+	}
+
+	public void setWürfel3(int würfel3) {
+		this.würfel3 = würfel3;
+	}
+
+	public int getWürfel4() {
+		return würfel4;
+	}
+
+	public void setWürfel4(int würfel4) {
+		this.würfel4 = würfel4;
+	}
+
+	public int getWürfel5() {
+		return würfel5;
+	}
+
+	public void setWürfel5(int würfel5) {
+		this.würfel5 = würfel5;
+	}
+	}
+
+
