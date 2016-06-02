b1c6749a2455a992ceb094fc4d259317c6e826e6
Alexander Cimander <alexander.cimander@de.ibm.com>
2016-06-02 09:00:27 +0200

    Startklasse

---
+++ Kniffel/src/kniffel/Kniffel/KniffelSpiel.java
@@ -0,0 +1,19 @@
+package kniffel.Kniffel;
+
+import javax.swing.JOptionPane;
+
+public class KniffelSpiel {
+	
+private int anzahlSpiele;
+
+
+
+public void spielStart(int spieler){
+	if(spieler==0){
+	JOptionPane.showMessageDialog(null,"Keine Spieler und Würfel vorhanden");
+	}else{
+		//Laden der Main Gui und start mit Spieler 1!!!
+	}
+}
+
+}
