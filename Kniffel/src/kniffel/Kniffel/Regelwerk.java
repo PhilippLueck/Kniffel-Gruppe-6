74f78a752440d36f1a4c120de2dd66de1df07be6
Alexander Cimander <alexander.cimander@de.ibm.com>
2016-06-09 13:28:59 +0200

    Regelwerk 1

--- Kniffel/src/kniffel/Kniffel/Regelwerk.java
+++ Kniffel/src/kniffel/Kniffel/Regelwerk.java
@@ -1,18 +1,136 @@
 package kniffel.Kniffel;
 
+import java.util.Iterator;
+
+
 public class Regelwerk extends KniffelSpiel {
-	
+
+int zahlenCount;
+int punkte;
+
 //1er
+public int einser(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
+	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 1;
+	}
+	
+	return punkte;
+}
 	
 //2er
+public int zweier(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
+	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 2;
+	}
+	
+	return punkte;
+}
 	
 //3er
+public int dreier(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
 	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 3;
+	}
+	
+	return punkte;
+}	
 //4er
+public int vierer(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
 	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 4;
+	}
+	
+	return punkte;
+}
 //5er
+public int fuenfer(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
+	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 5;
+	}
+	
+	return punkte;
+}
 	
 //6er
+public int sechser(){
+	//Punkte zurücksetzen
+	punkte =0;
+	//jetzt Liste durchlaufen
+	Iterator<Würfel> WürfelIterator = KniffelSpiel.würfelListe.iterator();		
+	while(WürfelIterator.hasNext()){
+		Würfel selectedWürfel = WürfelIterator.next();
+		if(selectedWürfel.augenzahl==1){
+			zahlenCount++;
+		}
+	}//Ende While
+	
+	//Jetzt Punkte zusammenzählen
+	for(int j = 1;j<=zahlenCount;j++){
+	punkte = punkte + 6;
+	}
+	
+	return punkte;
+}
 	
 //pasch
 	
